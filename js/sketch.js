67c0d4164a003389ec9d21278ade09a73dce4f61
Niels NTG <NTG999@gmail.com>
2019-01-26 00:11:30 +0100

    Implement basic movement

--- js/sketch.js
+++ js/sketch.js
@@ -1,16 +1,77 @@
+let player;
+let counterTop;
+let microwave;
 
+let kitchenObjects;
+let kitchenClutter;
+
+const counterHeight = 40;
+
+const GRAVITY = 1;
+const JUMP = 15;
+const ACCELERATION = 10;
+const MAX_SPEED = 10;
+const FRICTION = 1;
 
 function preload() {
 
 }
 
 function setup() {
-    createCanvas(displayWidth, displayHeight);
+    createCanvas(windowWidth, windowHeight);
+
+    player = createSprite(0, 0, 100, 100);
+    player.setDefaultCollider();
+
+    counterTop = createSprite(width / 2, height, width, counterHeight);
+    counterTop.setDefaultCollider();
+
+    microwave = createSprite(width / 2 + 400, height - 60, 100, 80);
+    microwave.setDefaultCollider();
+
+    kitchenObjects = new Group();
+    kitchenObjects.add(counterTop);
+    kitchenObjects.add(microwave);
+
+    kitchenClutter = new Group();
+    kitchenClutter.add(microwave);
+
 }
 
 function draw() {
     clear();
 
-    fill(random(255));
-    square(mouseX, mouseY, 40, 40);
+    applyMovement();
+
+    drawSprites();
+}
+
+function applyMovement() {
+
+    player.velocity.y += GRAVITY;
+
+    player.velocity.x = 0;
+
+    if (player.collide(kitchenObjects)) {
+        player.velocity.y = 0;
+    }
+
+    if (keyWentDown('space') && player.position.y > 0 && player.overlap(kitchenObjects)) {
+        player.velocity.y = -JUMP;
+    }
+    if (
+        (
+            keyDown('a') || keyDown(LEFT_ARROW)
+        ) &&
+        player.position.x > 0
+    ) {
+        player.velocity.x = -MAX_SPEED;
+    } else if ((
+        keyDown('d') || keyDown(RIGHT_ARROW)
+    ) &&
+        player.position.x < width
+    ) {
+        player.velocity.x = MAX_SPEED;
+    }
+
 }
